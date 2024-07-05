0c4185bd897601a6d3936ab4b302d98f2f3138c8
Awais <awais.3utt@gmail.com>
2024-07-05 16:38:54 +0500

    faker install

--- cypress/e2e/pages/P01_RegisterUser.js
+++ cypress/e2e/pages/P01_RegisterUser.js
@@ -1,13 +1,11 @@
 import {RegisterUserLocators} from "../locators/RegisterUser.json"
+const { faker } = require('@faker-js/faker');  //USED FAKER FOR RANDOM FirstName, LastName, Email, Telephone
 
 export class P01_RegisterUserClass{
     MyAccount(){
         cy.xpath(RegisterUserLocators.MyAccount).trigger('mouseover')
-        cy.xpath(RegisterUserLocators.Register).click
+        cy.xpath(RegisterUserLocators.Register).click()
 
-    }
-    Register(){
-        
     }
     FirstName(){
         
